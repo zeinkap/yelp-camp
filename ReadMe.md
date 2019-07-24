f2cfdd840aa8cf0871ff7a275f450801617223bd
zeinkap <zeinkap@gmail.com>
2019-07-24 18:52:05 -0400

    first commit where all 7 restful routes have beeen added, but not fully implemented front-end content

---
+++ ReadMe.md
@@ -0,0 +1,43 @@
+# YelpCamp
+* Add Landing Page
+* Add campgrounds page that lists all campgrounds
+* In future database will be added
+
+# Each campground will have:
+* Name
+* Image
+
+# For layout and styling
+* Create our header and footer partials
+* Add in Bootstrap
+
+# Creating new campgrounds
+* Setup new campground POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
+# Style the campgrounds page
+* Add better header/title
+* Move campgrounds display in a grid
+
+# Style the Navbar and form
+* Add navbar to all templates
+* Style new campground form
+
+# Add Mongoose
+* Install and configure mongoose
+* Setup campground model
+* Use campground model inside of our routes!
+
+# Show page
+* Review the RESTful routes
+* Add description to our campground model
+* Show db.collection.drop()
+* Add a show route/template
+
+# Different types of Restful Routes:
+- INDEX which is like /dogs and is GET request that displays a list of all dogs
+- NEW which is /dogs/new GET request that displays form to make a new dog
+- CREATE which is /dogs POST request that adds new dog to DB
+- SHOW /dogs/:id GET request that shows info about one dog
---
+++ app.js
@@ -0,0 +1,138 @@
+const express = require("express"),
+	  app = express(),
+	  bodyParser = require("body-parser"),
+	  mongoose = require("mongoose");
+
+// mongoose.connect('mongodb+srv://zeinkap:Zkap9611@cluster0-f0jxn.mongodb.net/test?retryWrites=true&w=majority', {
+// 	useNewUrlParser: true,
+// 	useCreateIndex: true
+// }).then(() => {
+// 		console.log('Connected to DB');
+// }).catch((err) => {
+// 	console.log('Error: ', err.message);
+// });
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});	// to get rid of warning errors
+//app.use(express.static("public"));	//used to serve your own custom css 
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs"); 
+
+// MONGOOSE/MODEL CONFIG
+const campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+//adds campground to DB
+// Campground.create(
+// 	{
+// 		name: "Granite Hill",
+// 		image: "https://assets.simpleviewinc.com/simpleview/image/fetch/q_60/https://assets.simpleviewinc.com/simpleview/image/upload/crm/poconos/Waterfalls-Bushkill-Falls-3-PoconoMtns0_6c255fcc-a099-e9fa-f98f91bcfd3d7649.jpg",
+// 		description: "This place is huge, but no bathrooms. The scenery is pretty nice and relaxing."
+// 	}, (err, campground) => {
+// 	if(err) {
+// 		console.log("There is an error!");
+// 		console.log(err);
+// 	} else {
+// 		console.log("Newly created campground: ");
+// 		console.log(campground);
+// 	}
+// });
+
+
+app.get("/", (req, res) => {
+    res.redirect("/campgrounds");
+});
+
+// 1. INDEX route - shows all campgrounds
+app.get("/campgrounds", (req, res) => {
+	//rather than getting from array we will retrieve camps from DB
+	Campground.find({}, (err, campgrounds) => {	//campgrounds here is placeholder for the data coming back from DB from out .find
+	if(err) {
+		console.log("There is an error!");
+		console.log(err);
+	} else {
+		res.render("index", {campgrounds : campgrounds});	//rendering index file along with the data sent under the first name campgrounds
+	}
+	});
+});
+
+// 2. NEW route - shows form to create new campground
+app.get("/campgrounds/new", (req, res) => { //this route is where form will be shown
+    res.render("new");
+});
+
+// 3. CREATE route - add new campground to DB
+app.post("/campgrounds", (req, res) => {     //not same as GET, we are following REST convention format
+    //get data from form
+    let name = req.body.name;
+	let image = req.body.image;
+	let description = req.body.description;
+	let newCampground = {name: name, image: image, description: description};	//created object 
+	// Create a new campground and save to DB
+	Campground.create(newCampground, (err, newlyCreated) => {
+		if(err) {
+			console.log("There is an error!");
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");   //event though there are 2 of these routes, it will redirect to GET by default
+		}			  
+	});
+});
+
+// 4. SHOW route - shows info about a specific campground. ***This should always come after NEW route or else it will overide it. id comes from mongo DB
+app.get("/campgrounds/:id", (req, res) => {	
+	//find campground with provided ID from url and use mongoose function findbyid
+	Campground.findById(req.params.id, (err, foundCampground) => {	//foundCampground serves as placeholder for our data that we get back from DB
+		if(err) {
+			console.log("There is an error!");
+			console.log(err);
+		} else {
+			//render show template with that campground
+			res.render("show", {campground: foundCampground});	//campground is just name we give, more importantly the value of id we found will be displayed
+		}
+	});
+});
+
+// 5. EDIT route - editing a campground
+app.get("/campgrounds/:id/edit", (req, res) => {
+	Campground.findById(req.params.id, (err, foundCampground) => {
+		if(err) {
+			console.log("There is an error!");
+			console.log(err);
+		} else {
+			res.render("edit", {campground: foundCampground});
+		}
+	});
+});
+
+// 6. UPDATE route - updating details of an existing campground
+app.put("/campgrounds/:id", (req, res) => {
+	Campground.findByIdAndUpdate(req.params.id, (err, updateCampground) => {
+		if(err) {
+			console.log("There is an error!");
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+// 7. DESTROY route - Removing campground 
+app.delete("/campgrounds/:id", (req, res) => {
+	Campground.findByIdAndRemove(req.params.id, (err) => {
+		if(err) {
+			console.log("There is an error!");
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+app.listen(3000, () => {
+	console.log("Listening to port 3000");
+});
