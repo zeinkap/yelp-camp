e10ade3c99962f9c029afe74c119ddc47f253a6e
zeinkap <zeinkap@gmail.com>
2019-09-19 13:50:27 -0400

    updated mongoose to connect to mongoDB Atlas

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const	commentRoutes			= require("./routes/comments"),
 		indexRoutes				= require("./routes/index")
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://zeinkap:Zkap9611@cluster0-f0jxn.mongodb.net/test?retryWrites=true&w=majority")
+//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(express.static(__dirname + "/public"));	// tells express to look in public directory for custom stylesheets. dirname refers to root YelpCamp folder 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));	//argument is what to look for in url
